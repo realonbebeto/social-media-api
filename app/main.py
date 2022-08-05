7ccd657038e27fbac6a35bb8af35f5a627b074e9
realonbebeto <projobs254@gmail.com>
2022-08-05 12:59:30 +0300

    cors update

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, like
 
 # Not neccessary when alembic is setup
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router, prefix='/posts')
 app.include_router(user.router, prefix='/users')
